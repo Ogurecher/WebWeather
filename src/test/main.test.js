fca7c706729456c63b1766a199e48fed172381cd
Dmitrii Peredreev <ddm157@yandex.ru>
2019-11-12 13:51:39 +0300

    Trying to figure out tests

--- src/test/main.test.js
+++ src/test/main.test.js
@@ -50,40 +50,115 @@ const eventMock = {
   }]
 };
 
-const contextMock = {};
 
-let spy = sinon.spy(console, 'log');
+
+const spy = sinon.spy(console, 'log');
 
 describe('getWeather', () => {
   it('at least starts', () => {
-
     const {getWeather} = require("../main.js");
 
     getWeather(eventMock);
 
     assert(spy.calledWith('getWeather'));
-  })
+  });
+
+  it('sends API request', () => {
+
+  });
+
+  it('gets API response', () => {
+
+  });
+
+  it('drops empty responses', () => {
+
+  });
+
+  it('removes previous layout if present', () => {
+
+  });
+
+  it('calls renderError if API response cotains an error', () => {
+
+  });
+
+  it('calls renderContent if OK', () => {
+
+  });
 });
 
 describe('renderContent', () => {
+  const {renderContent} = require("../main.js");
+
   it('at least starts', () => {
-    
-
-    const {renderContent} = require("../main.js");
-
     renderContent(responseXMLMock);
 
     assert(spy.calledWith('renderContent'));
-  })
+  });
+  
+  it('populates context', () => {
+    const { template } = require('../main');
+    const spyTemplate = sinon.spy(template);
+
+    const contextMock = {
+      weatherDesc: '',
+      weatherIcon: '',
+      temperature: '',
+      sunrise: '',
+      sunset: '',
+      rain: '',
+      humidity: '',
+      windDir: '',
+      windSpd: '',
+      sunriseIcon: 'http://icons.iconarchive.com/icons/iconsmind/outline/64/Sunrise-icon.png',
+      windIcon: 'https://image.flaticon.com/icons/png/128/184/184971.png'
+    };
+
+    const responseXMLMock = {
+      getElementsByTagName: (name) => eval(`responseXMLMock.${name}`),
+      weatherDesc: [{textContent: ''}],
+      weatherIconUrl: [{textContent: ''}],
+      temp_C: [{textContent: ''}],
+      sunrise: [{textContent: ''}],
+      sunset: [{textContent: ''}],
+      chanceofrain: [{textContent: ''}],
+      humidity: [{textContent: ''}],
+      winddir16Point: [{textContent: ''}],
+      windspeedKmph: [{textContent: ''}]
+    };
+
+    renderContent(responseXMLMock);
+
+    assert(spyTemplate.called);
+    //assert(spy.calledWith(contextMock));
+  });
+
+  it('creates div to render', () => {
+
+  });
+
+  it('appends div to document.main', () => {
+
+  });
 });
 
 describe('renderError', () => {
   it('at least starts', () => {
-
     const {renderError} = require("../main.js");
+
+    const contextMock = {};
 
     renderError(contextMock);
 
     assert(spy.calledWith('renderError'));
-  })
+  });
+
+  it('creates div to render', () => {
+
+  });
+
+  it('appends div to document.main', () => {
+
+  });
 });
