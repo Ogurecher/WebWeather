0de382f7907cffbef08b4e6bc9ccea994e389ba3
Dmitrii <1@1.1>
2020-01-14 18:12:36 +0300

    Fixed async + event.preventDefault() interaction

--- src/main.js
+++ src/main.js
@@ -4,9 +4,8 @@ const source = document.getElementById('handlebarsTemplate').innerHTML;
 const template = Handlebars.compile(source);
 const errorTemplate = Handlebars.compile(document.getElementById('errorTemplate').innerHTML);
 
-getWeather=async (event) => {
-    event.preventDefault();
-    await fetch(weatherUrl+'?key='+apiKey+'&q='+event.target[0].value+'&num_of_days=1&format=json')
+getWeather=  (target) => {
+     fetch(weatherUrl+'?key='+apiKey+'&q='+target[0].value+'&num_of_days=1&format=json')
     .then(response => response.json())
     .then((data) => {
 
@@ -19,7 +18,7 @@ getWeather=async (event) => {
             /*console.log('error detected');
             console.log({city: event.target[0].value});
             console.log(renderError);*/
-            renderError({city: event.target[0].value});
+            renderError({city: target[0].value});
         } else {
             renderContent(formContext(data.data));
         };
@@ -55,7 +54,6 @@ renderContent=(context) => {
 };
 
 renderError=(context) => {
-    //console.log('entered renderError');
     const main = document.getElementById('main');
     const html = errorTemplate(context);
     const div= document.createElement('div');
@@ -63,10 +61,13 @@ renderError=(context) => {
     div.id = 'outputLayout';
 
     main.appendChild(div);
-    //bconsole.log('exiting renderError');
 };
 
-document.getElementById('inputForm').addEventListener('submit', getWeather);
+document.getElementById('inputForm').addEventListener('submit', (event) => {
+    event.preventDefault();
+    getWeather(event.target);
+});
+
 module.exports = {
     getWeather: getWeather,
     renderContent: renderContent,
